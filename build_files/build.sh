111690e1614abeeb53455ab3047b275ded0932df
Simeon <simeon-walker@users.noreply.github.com>
2025-06-22 12:19:49 +0100

    Add cosmic copr to build

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,9 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 -y copr enable ryanabx/cosmic-epoch
+dnf5 -y install cosmic-desktop
+dnf5 -y copr disable ryanabx/cosmic-epoch
 
 # Use a COPR Example:
 #
@@ -21,4 +23,4 @@ dnf5 install -y tmux
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+# systemctl enable podman.socket
